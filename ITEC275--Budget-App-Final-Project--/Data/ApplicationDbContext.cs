d190aa0d30506670ee53dd1d2895804711345d2e
Thomas Gander - School <thomas.gander@mymhc.ca>
2024-03-25 11:24:11 -0600

    Add project files.

---
+++ ITEC275--Budget-App-Final-Project--/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITEC275__Budget_App_Final_Project__.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ ITEC275--Budget-App-Final-Project--/Program.cs
@@ -0,0 +1,74 @@
+using ITEC275__Budget_App_Final_Project__;
+using ITEC275__Budget_App_Final_Project__.Areas.Identity;
+using ITEC275__Budget_App_Final_Project__.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.JSInterop;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+//builder.Services.AddScoped<ToDoAppDbContext>();
+builder.Services.AddScoped<Popups>(sp =>
+{
+    var jsRuntime = sp.GetRequiredService<IJSRuntime>();        //Needed help from ChatGPT to figure out how to instatiate this using the JSRuntime.
+    return new Popups(jsRuntime);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+//Needed to assign/create roles
+//using (var serviceScope = app.Services.CreateScope())
+//{
+//    var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+//    if (!await roleManager.RoleExistsAsync("Admin"))        //Not sure if needed
+//    {
+//        await roleManager.CreateAsync(new IdentityRole("Admin"));
+//    }
+
+//    if (!await roleManager.RoleExistsAsync("User"))
+//    {
+//        await roleManager.CreateAsync(new IdentityRole("User"));
+//    }
+//}
+
+app.Run();
